3e1c571759cd2333b239713ef34edf58927365ca
OmarAp <o.aparicio@ebc.edu.mx>
2021-10-28 17:13:12 -0500

    step/1 Add method to init chain

--- blockchain.py
+++ blockchain.py
@@ -8,3 +8,15 @@ class Blockchain:
         self.chain = []
         self.nodes = set()
         self.new_block(previous_hash='1', proof=100)
+
+    def new_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
